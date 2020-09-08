966b5f785a8fcfe4f17ca849089f99f66c6fcfb1
Siddharth Khandelwal <58730831+khandelwal20sid@users.noreply.github.com>
2020-09-08 12:37:52 +0530

    Add files via upload

---
+++ Day 4/Que1.js
@@ -0,0 +1,18 @@
+function originalpic() {
+let img = document.getElementById("image")
+let newurl="https://assets1.ignimgs.com/2020/02/14/marvels-avengers---button-2020-1581705831993.jpg";
+img.src=newurl;
+}
+
+function changepic1() {
+let img = document.getElementById("image");
+let newurl="https://bgr.com/wp-content/uploads/2019/11/avengers-endgame-iron-man-gauntlet.jpg?quality=70&strip=all";
+img.src= newurl;
+}
+
+
+function changepic2() {
+    let img = document.getElementById("image");
+    let newurl="https://o.aolcdn.com/images/dims?quality=95&image_uri=https%3A%2F%2Fs.yimg.com%2Fuu%2Fapi%2Fres%2F1.2%2Fy6hTz6Yb_abs_6rwzFvFtg--%7EB%2FaD05MDA7dz0xNjAwO2FwcGlkPXl0YWNoeW9u%2Fhttps%3A%2F%2Fo.aolcdn.com%2Fimages%2Fdims%3Fresize%3D2000%252C2000%252Cshrink%26image_uri%3Dhttps%253A%252F%252Fs.yimg.com%252Fos%252Fcreatr-uploaded-images%252F2019-08%252F43116370-c36f-11e9-a1bf-373262e5fbb8%26client%3Da1acac3e1b3290917d92%26signature%3D2c744779d555189575cb514dca4c5c47a5b9b55c&client=amp-blogside-v2&signature=ce723e054bd6f629ae79d01517d060dfe0c5bfa3";
+    img.src= newurl;
+}
